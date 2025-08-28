de6d88534c35836be04f3db8abe7b39a0aa9b918
Maddie Lowe <maddielowe@sc.rr.com>
2025-08-28 18:29:59 -0400

    Added way to add and store multiple students

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include "student_profile.h"
 
 int main() {
-
- StudentProfile student;
+    
+    vector<StudentProfile> studentProfiles;
+    char choice;
 
     cout << "Welcome to Study Buddy Profile Setup\n";
-    student.createProfile();
 
-    student.displayProfile();
+    do{
+        StudentProfile student;
+        student.createProfile();
+        studentProfiles.push_back(student);
+
+        cout << "\nAdd another student? Type y for yes n for no: ";
+        cin >> choice;
+        cin.ignore();
+    }
+    while(choice == 'y'|| choice == 'Y');
+
+    cout << "\nAll profiles created.\n";
+
+    for(size_t i = 0; i < studentProfiles.size(); ++i){
+        cout << "\nStudent #" << (i + 1) << ": \n";
+        studentProfiles[i].displayProfile();
+    }
 
 return 0;
 
