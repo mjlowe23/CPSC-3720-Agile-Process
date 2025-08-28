cff7e5685345cab91a7baed1e037a95bad046566
sbskinn <sbskinn@clemson.edu>
2025-08-28 17:53:41 -0400

    Header file changes

--- student_profile.h
+++ student_profile.h
@@ -6,15 +6,23 @@
 #include <iostream>
 using namespace std;
 
+struct TimeSlot {
+    string day;
+    string start;
+    string end;
+};
+
 class StudentProfile {
 private:
     string studentName;
     vector<string> courses;
+    vector<TimeSlot> availability;
 
 public:
     void createProfile();
     void displayProfile();
-
+    void addAvailability(const TimeSlot& slot)
+    const vector<TimeSlot>& getAvailability() const;
 };
 
 #endif
