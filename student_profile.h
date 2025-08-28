be32bce94a98a619e366dd7fb1fd74f439fde643
Noelle Gayer <ngayer@clemson.edu>
2025-08-28 18:00:05 -0400

    Add functions to header file to implement user story 4 and 5

--- student_profile.h
+++ student_profile.h
@@ -17,12 +17,23 @@ private:
     string studentName;
     vector<string> courses;
     vector<TimeSlot> availability;
+    vector<string>availableTimeSlots;
+    vector<string> proposedSessions;
 
 public:
     void createProfile();
     void displayProfile();
-    void addAvailability(const TimeSlot& slot)
+    void addAvailability(const TimeSlot& slot);
     const vector<TimeSlot>& getAvailability() const;
+
+    // time slot function (user story 4)
+    void addAvailableTimeSlots();
+    void displayAvailableTimeSlots();
+
+    // time slot function (user story 5)
+    void proposeStudySession(const string& classmateName, const string& timeSlot);
+    void displayProposedSessions();
+
 };
 
 #endif
